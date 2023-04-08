e258545e05b828660e0f0feee398c01162073fa7
Sergcode <shsv100@ya.ru>
2023-04-08 16:57:00 +0700

    Уменьшил код путем создания компонента из повторяющегося кода. Инфа об пользователе и две кнопки.

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -3,58 +3,16 @@
     <div class="banner">
       <div class="container" v-if="article">
         <h1>{{ article.title }}</h1>
-
-        <div class="article-meta">
-          <router-link
-            :to="{name: 'userProfile', params: {slug: article.author.username}}"
-          >
-            <img :src="article.author.image" alt="" />
-          </router-link>
-          <div class="info">
-            <router-link
-              class="author"
-              :to="{
-                name: 'userProfile',
-                params: {slug: article.author.username}
-              }"
-            >
-              {{ article.author.username }}
-            </router-link>
-            <!--.createdAt - взят из Сети в инструменте разраба в объекте article-->
-            <span class="date">{{ article.createdAt }}</span>
-          </div>
-
-          <span v-if="isAuthor">
-            <router-link
-              class="btn btn-outline-secondary btn-sm"
-              :to="{name: 'editArticle', params: {slug: article.slug}}"
-            >
-              <i class="ion-edit" />
-              Edit Article
-            </router-link>
-            &nbsp;
-            <button
-              class="btn btn-outline-danger btn-sm"
-              @click="deleteArticle"
-            >
-              <i class="ion-trash-a" />
-              Delete Article
-            </button>
-          </span>
-
-          <span v-else>
-            <button class="btn btn-sm btn-secondary">
-              <i class="ion-plus-round"></i>
-              &nbsp;Unfollow {{ article.author.username }}
-            </button>
-            &nbsp;
-            <mcv-add-to-favorites
-              :is-favorited="article.favorited"
-              :article-slug="article.slug"
-              :favorites-count="article.favoritesCount"
-            />
-          </span>
-        </div>
+        <mcv-user-info-and-buttons
+          :article-author-username="article.author.username"
+          :article-author-image="article.author.image"
+          :article-created-at="article.createdAt"
+          :is-author="isAuthor"
+          :article-slug="article.slug"
+          :delete-article="deleteArticle"
+          :article-favorited="article.favorited"
+          :article-favorites-count="article.favoritesCount"
+        />
       </div>
     </div>
     <div class="container page">
@@ -72,58 +30,17 @@
 
       <hr />
 
-      <div class="article-actions">
-        <div class="article-meta">
-          <router-link
-            :to="{name: 'userProfile', params: {slug: article.author.username}}"
-          >
-            <img :src="article.author.image" alt="" />
-          </router-link>
-          <div class="info">
-            <router-link
-              class="author"
-              :to="{
-                name: 'userProfile',
-                params: {slug: article.author.username}
-              }"
-            >
-              {{ article.author.username }}
-            </router-link>
-            <!--.createdAt - взят из Сети в инструменте разраба в объекте article-->
-            <span class="date">{{ article.createdAt }}</span>
-          </div>
-
-          <span v-if="isAuthor">
-            <router-link
-              class="btn btn-outline-secondary btn-sm"
-              :to="{name: 'editArticle', params: {slug: article.slug}}"
-            >
-              <i class="ion-edit" />
-              Edit Article
-            </router-link>
-            &nbsp;
-            <button
-              class="btn btn-outline-danger btn-sm"
-              @click="deleteArticle"
-            >
-              <i class="ion-trash-a" />
-              Delete Article
-            </button>
-          </span>
-
-          <span v-else>
-            <button class="btn btn-sm btn-secondary">
-              <i class="ion-plus-round"></i>
-              &nbsp;Unfollow {{ article.author.username }}
-            </button>
-            &nbsp;
-            <mcv-add-to-favorites
-              :is-favorited="article.favorited"
-              :article-slug="article.slug"
-              :favorites-count="article.favoritesCount"
-            />
-          </span>
-        </div>
+      <div class="article-actions" v-if="article">
+        <mcv-user-info-and-buttons
+          :article-author-username="article.author.username"
+          :article-author-image="article.author.image"
+          :article-created-at="article.createdAt"
+          :is-author="isAuthor"
+          :article-slug="article.slug"
+          :delete-article="deleteArticle"
+          :article-favorited="article.favorited"
+          :article-favorites-count="article.favoritesCount"
+        />
       </div>
     </div>
   </div>
@@ -136,7 +53,7 @@ import {mapState, mapGetters} from 'vuex'
 import McvLoading from '@/components/Loading'
 import McvErrorMessage from '@/components/ErrorMessage'
 import McvTagList from '@/components/TagList'
-import McvAddToFavorites from '@/components/AddToFavorites'
+import McvUserInfoAndButtons from '@/components/UserInfoAndButtons'
 
 export default {
   name: 'McvArticle',
@@ -144,7 +61,7 @@ export default {
     McvLoading,
     McvErrorMessage,
     McvTagList,
-    McvAddToFavorites
+    McvUserInfoAndButtons
   },
   computed: {
     ...mapState({
