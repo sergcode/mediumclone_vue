d73d017916495ca19065e402a07eeae0b6691b27
Sergcode <shsv100@ya.ru>
2023-04-08 16:58:02 +0700

    change class articles-toggle

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -30,7 +30,7 @@
     <div class="container">
       <div class="row">
         <div class="col-xs-12 col-md-10 offset-md-1">
-          <div class="article-toggle">
+          <div class="articles-toggle">
             <ul class="nav nav-pills outline-active">
               <li class="nav-item">
                 <router-link
